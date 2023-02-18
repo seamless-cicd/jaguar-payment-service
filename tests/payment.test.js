a40696b58e2e1e2798538295bb8a13aa7f205ee0
RDeJonghe <rdejonghe@hotmail.com>
2023-02-18 12:13:01 -0500

    feat: added more unit tests
    unit tests added to have more tests

--- tests/payment.test.js
+++ tests/payment.test.js
@@ -10,11 +10,44 @@ describe('GET /', () => {
     );
   });
 
+  /* EXAMPLE TEST FAILURE
   it('fails a test', async () => {
     const response = await request(app).get('/');
     expect(response.statusCode).toBe(200);
     expect(response.text).toBe(
       'You have reached the Panther payment service. Please send a POST request to /payment (no body required).'
     );
+  });
+  */
+});
+
+describe('POST /payment', () => {
+  it('responds with a message', async () => {
+    const response = await request(app).post('/payment');
+    expect(response.statusCode).toBe(200);
+    expect(response.body.message).toBe(
+      'You have paid protection money to the Jaguars. Your safety is guaranteed for now!'
+    );
+  });
+
+  it('responds with a payment amount', async () => {
+    const response = await request(app).post('/payment');
+    expect(response.statusCode).toBe(200);
+    expect(response.body.amountPaid).toBe(1000);
+  });
+});
+
+describe('GET /payment', () => {
+  it('responds with a message', async () => {
+    const response = await request(app).get('/payment');
+    expect(response.statusCode).toBe(200);
+    expect(response.body.message).toBe('The payment for protection is due');
+    // expect(response.body.paymentAmount).toBe(1000);
+  });
+
+  it('responds with amount due', async () => {
+    const response = await request(app).get('/payment');
+    expect(response.statusCode).toBe(200);
+    expect(response.body.amountDue).toBe(1000);
   });
 });
