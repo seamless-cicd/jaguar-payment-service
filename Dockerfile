cad49bfd41d683f0d522a4110642efed82b45dab
Jason Wang <jasonherngwang@gmail.com>
2023-02-23 07:05:20 -0800

    chore: make change to test pipeline

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM --platform=linux/amd64 node:16
+FROM --platform=linux/amd64 node:18-bullseye-slim
 WORKDIR /usr/src/app
 COPY package*.json ./
 RUN npm ci --only=production
--- app.js
+++ app.js
@@ -9,20 +9,23 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res
     .status(200)
-    .send('You have reached the Jaguar payment service. Please send a POST request to /payment (no body required).');
+    .send(
+      'You have reached the Jaguar payment service. Please send a POST request to /payment (no body required)!'
+    );
 });
 
 app.get('/payment', (req, res) => {
   res.status(200).send({
     message: 'The payment for protection is due',
-    amountDue: 1000
+    amountDue: 1000,
   });
 });
 
 app.post('/payment', (req, res) => {
   res.status(200).send({
-    message: 'You have paid protection money to the Jaguars. Your safety is guaranteed for now!',
-    amountPaid: 1000
+    message:
+      'You have paid protection money to the Jaguars. Your safety is guaranteed for now!',
+    amountPaid: 1000,
   });
 });
 
